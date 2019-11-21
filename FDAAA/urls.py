d3477934204a442951c012acc1b66665730eecd9
hc <hc@hcdeMacBook-Pro.local>
2019-11-21 19:38:48 +0800

    conf uwsgi

--- FDAAA/urls.py
+++ FDAAA/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('galaxy/', include('galaxy.urls'))
+    path('galaxy/', include('galaxy.urls')),
 ]
